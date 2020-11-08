28e4be8b5521bd37c785434796a7b22fa6a3f5a3
Paulius <paulius.uzusenis@gmail.com>
2020-11-08 15:53:46 +0200

    Updates

---
+++ precache-manifest.5b1b5732652ef82f7d88e5fe2579527b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "647f51b70ad862d7695d46dba513f310",
+    "url": "/pokefriends/index.html"
+  },
+  {
+    "revision": "c9efd8f1de297e0a7d12",
+    "url": "/pokefriends/static/css/2.038644eb.chunk.css"
+  },
+  {
+    "revision": "5e582ad4c856b2efd62e",
+    "url": "/pokefriends/static/css/main.ff673a96.chunk.css"
+  },
+  {
+    "revision": "c9efd8f1de297e0a7d12",
+    "url": "/pokefriends/static/js/2.469b6bb6.chunk.js"
+  },
+  {
+    "revision": "c50c771658c38c7c1fcfc5b118622f46",
+    "url": "/pokefriends/static/js/2.469b6bb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e582ad4c856b2efd62e",
+    "url": "/pokefriends/static/js/main.f75d597a.chunk.js"
+  },
+  {
+    "revision": "c666e9941f2bdccb7457",
+    "url": "/pokefriends/static/js/runtime-main.c62d027f.js"
+  },
+  {
+    "revision": "510f8c1e1db5baf7478da8dbcf1044e3",
+    "url": "/pokefriends/static/media/Pokemon_Solid.510f8c1e.woff"
+  }
+]);
