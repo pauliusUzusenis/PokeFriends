28114fad805c5f7570f5b13aab11d7d5f4f36f5f
Paulius <paulius.uzusenis@gmail.com>
2024-01-16 14:37:35 +0200

    fix card image source url

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Card = ({ name, id, toggleModal }) => {
 	return (
 		<div className='tc card dib br3 pa4 ma3 grow bw2 shadow-4' onClick={() => toggleModal(id) }>
-			<img alt='admin' src={`https://pokeres.bastionbot.org/images/pokemon/${id}.png`} />
+			<img alt='admin' src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`} />
 			<div>
 				<h2 className='ttc'>{name}</h2>
 			</div>
